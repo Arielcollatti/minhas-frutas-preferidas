1bd10b07bdab45fb1382e66a31bbd6f17d76d8f3
arielcollatti <colatti2017@gmailcom>
2023-03-06 09:54:05 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("minhas frutas preferidas");
+Console.WriteLine();
+Console.WriteLine("banana");
+Console.WriteLine("morango");
+Console.WriteLine("melancia");
+Console.WriteLine("abacate");
+Console.WriteLine("uva");
+
+
+
+
+
+
+
